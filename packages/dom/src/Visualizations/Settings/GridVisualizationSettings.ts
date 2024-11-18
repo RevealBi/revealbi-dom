edc694d247f620b55cd52e27a8cfe950f08f2a39
Brian Lagunas <835562+brianlagunas@users.noreply.github.com>
2024-11-18 11:08:50 -0700

    added paging support to grid viz

--- packages/dom/src/Visualizations/Settings/GridVisualizationSettings.ts
+++ packages/dom/src/Visualizations/Settings/GridVisualizationSettings.ts
@@ -1,5 +1,6 @@
 import { SchemaTypeNames } from "../../Core/Constants/SchemaTypeNames";
 import { VisualizationTypes } from "../../Core/Constants/VisualizationTypes";
+import { JsonProperty } from "../../Core/Serialization/Decorators/JsonProperty";
 import { GridVisualizationSettingsBase } from "./GridVisualizationSettingsBase";
 
 export class GridVisualizationSettings extends GridVisualizationSettingsBase {
@@ -10,16 +11,30 @@ export class GridVisualizationSettings extends GridVisualizationSettingsBase {
     }
 
     /**
+     * Gets or sets whether the grid should have paging enabled.
+     * Paging is supported only when ProcessDataOnServer is set to true and is not compatible with Data Blending.
+     * Supported data sources include: Athena, BigQuery, MySQL, Oracle, PostgreSQL, SQL Server, and SyBase.
+     */
+    @JsonProperty("PagedRows")
+    isPagingEnabled: boolean = true;
+
+    /**
+     * Gets or sets the number of rows to display in the grid.
+     */
+    @JsonProperty("PagedRowsSize")
+    pageSize: number = 50;
+
+    /**
      * Gets whether the first column in the grid is fixed.
      */
-    get fixFirstColumn(): boolean {
+    get isFirstColumnFixed(): boolean {
         return this.style.fixedLeftColumns;
     }
 
     /**
      * Sets whether the first column in the grid is fixed.
      */
-    set fixFirstColumn(value: boolean) {
+    set isFirstColumnFixed(value: boolean) {
         this.style.fixedLeftColumns = value;
     }
 }
