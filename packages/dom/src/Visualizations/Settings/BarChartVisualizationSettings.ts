6ce99d6f1b569d6257bdc0abc750f0513a6b247f
Brian Lagunas <835562+brianlagunas@users.noreply.github.com>
2025-03-26 16:32:06 -0600

    added gap and overlap properties

--- packages/dom/src/Visualizations/Settings/BarChartVisualizationSettings.ts
+++ packages/dom/src/Visualizations/Settings/BarChartVisualizationSettings.ts
@@ -1,3 +1,4 @@
+import { JsonProperty } from "../../Core/Serialization/Decorators/JsonProperty";
 import { RdashChartType } from "../Enums/ChartType";
 import { CategoryChartVisualizationSettings } from "./CategoryChartVisualizationSettings";
 
@@ -6,4 +7,16 @@ export class BarChartVisualizationSettings extends CategoryChartVisualizationSet
         super();
         this.chartType = RdashChartType.Bar;
     }
+
+    /**
+     * Gets or sets the gap between the bars in the chart. The value is a percentage of the bar width.
+     */
+    @JsonProperty("CategoryAxisGap")
+    public gap: number = 0.4;
+
+    /**
+     * Gets or sets the overlap between the bars in the chart. The value is a percentage of the bar width.
+     */
+    @JsonProperty("CategoryAxisOverlap")
+    public overlap: number = -0.2;
 }
